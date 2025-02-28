7852911812ce6b2894f06d05b7a0fd0fa3377db8
Joey Normil <animenatsulucy@gmail.com>
2025-02-28 03:47:35 -0500

    finished stubbed code, added makefile

---
+++ Makefile
@@ -0,0 +1,19 @@
+CC := gcc
+CFLAGS := -ggdb -Wall -Werror -O -std=c99 -D_DEFAULT_SOURCE
+
+all: csv
+
+csv: csv.o program.o
+	$(CC) $(CFLAGS) -o $@ $^
+
+csv.o: csv.c program.h
+
+program.o: program.c program.h
+
+%.o: %.c
+  $(CC) -o $@ -c $< $(CFLAGS)
+
+clean:
+	rm -f csv *.o
+
+.PHONY: all clean
--- program.c
+++ program.c
@@ -3,12 +3,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+// TODO: HANDLE CELLS IN QUOTES
 
 int is_valid_number(const char *str) {
     int i = 0, has_decimal = 0;
 
-    //TODO: HANDLE WHITESPACE
-    // HANDLE EMPTY CELL
+    if (strcmp(str, "") == 0) return 0;
 
     if (str[i] == '-') i++;
 
@@ -25,19 +25,51 @@ int is_valid_number(const char *str) {
 }
 
 int count_fields(const char *filename) {
-    return 0;
+    FILE *file = fopen(filename, "r");
+    if (!file) exit(EXIT_FAILURE);
+
+    char line[2048];
+    int count = 0;
+
+    if (fgets(line, sizeof(line), file) == NULL) {
+        fclose(file);
+        exit(EXIT_FAILURE);
+    }
+
+    fgets(line, sizeof(line), file);
+    char *field;
+    field = strtok(line, ",");
+    while (line) {
+        count++;
+        field = strtok(NULL, ",");
+    }
+
+    fclose(file);
+    return count;
 }
 
 int count_records(const char *filename, int has_header) {
-    return 0;
+    FILE *file = fopen(filename, "r");
+    if (!file) exit(EXIT_FAILURE);
+
+    char line[2048];
+    int count = 0;
+
+    if (fgets(line, sizeof(line), file) == NULL) {
+        fclose(file);
+        exit(EXIT_FAILURE);
+    }
+
+    while (fgets(line, sizeof(line), file)) count++;
+    
+    fclose(file);
+    return has_header? count - 1 : count;
 }
 
 double calculate_mean(const char *filename, int field_index) {
 
     FILE *file = fopen(filename, "r");
-    if (!file) {
-        exit(EXIT_FAILURE);
-    }
+    if (!file) exit(EXIT_FAILURE);
 
     char line[2048];
     double sum = 0.0;
@@ -70,41 +102,159 @@ double calculate_mean(const char *filename, int field_index) {
     }
 
     fclose(file);
-    if (count > 0) {
-        return (sum / count);
-    } else {
-        exit(EXIT_FAILURE);
-    }
+    if (count > 0) return (sum / count);
+    else exit(EXIT_FAILURE);
 }
 
 double calculate_max(const char *filename, int field_index) {
-    return 0.0;
+    FILE *file = fopen(filename, "r");
+    if (!file) exit(EXIT_FAILURE);
+
+    char line[2048];
+    double _max = 0.0;
+    int count = 0;
+
+    if (fgets(line, sizeof(line), file) == NULL) {
+        fclose(file);
+        exit(EXIT_FAILURE);
+    }
+
+    while (fgets(line, sizeof(line), file)) {
+        char *num_text;
+        int col = 0;
+
+        num_text = strtok(line, ",");
+        while (num_text) {
+
+            if (col == field_index) {
+
+                // check if number
+                if (is_valid_number(num_text)) {
+                    _max = max(_max, atof(num_text));
+                    count++;
+                }
+                break;
+            }
+            num_text = strtok(NULL, ",");
+            col++;
+        }
+    }
+
+    fclose(file);
+    if (count > 0)  return _max;
+    else exit(EXIT_FAILURE);
 }
 
 double calculate_min(const char *filename, int field_index) {
-    return 0.0;
+    FILE *file = fopen(filename, "r");
+    if (!file) exit(EXIT_FAILURE);
+
+    char line[2048];
+    double _min = 0.0;
+    int count = 0;
+
+    if (fgets(line, sizeof(line), file) == NULL) {
+        fclose(file);
+        exit(EXIT_FAILURE);
+    }
+
+    while (fgets(line, sizeof(line), file)) {
+        char *num_text;
+        int col = 0;
+
+        num_text = strtok(line, ",");
+        while (num_text) {
+
+            if (col == field_index) {
+
+                // check if number
+                if (is_valid_number(num_text)) {
+                    _min = min(_min, atof(num_text));
+                    count++;
+                }
+                break;
+            }
+            num_text = strtok(NULL, ",");
+            col++;
+        }
+    }
+
+    fclose(file);
+    if (count > 0) return _min;
+    else exit(EXIT_FAILURE);
 }
 
 double calculate_mean_by_name(const char *filename, const char *field_name) {
-    return 0.0;
+    int idx = get_field_index_by_name(filename, field_name);
+    return calculate_mean(filename, idx);
 }
 
 double calculate_max_by_name(const char *filename, const char *field_name) {
-    return 0.0;
+    int idx = get_field_index_by_name(filename, field_name);
+    return calculate_max(filename, idx);
 }
 
 double calculate_min_by_name(const char *filename, const char *field_name) {
-    return 0.0;
+    int idx = get_field_index_by_name(filename, field_name);
+    return calculate_min(filename, idx);
 }
 
 void display_records(const char *filename, int field_index, const char *value) {
-    return;
+    FILE *file = fopen(filename, "r");
+    if (!file) exit(EXIT_FAILURE);
+
+    char line[2048];
+
+    if (fgets(line, sizeof(line), file) == NULL) {
+        fclose(file);
+        exit(EXIT_FAILURE);
+    }
+
+    while (fgets(line, sizeof(line), file)) {
+        char *num_text;
+        num_text = strtok(line, ",");
+        int idx = 0;
+
+        while (line) {
+            if (idx == field_index && strcmp(value, num_text) == 0) {
+                printf("%s", line);
+                break;
+            }
+            idx++;
+            num_text = strtok(NULL, ",");
+        }
+    }
+    
+    fclose(file);
 }
 
 void display_records_by_name(const char *filename, const char *field_name, const char *value) {
-    return;
+    int idx = get_field_index_by_name(filename, field_name);
+    display_records(filename, idx, value);
 }
 
-int get_field_index_by_name(FILE *file, const char *field_name) {
-    return -1;
+int get_field_index_by_name(FILE *filename, const char *field_name) {
+    FILE *file = fopen(filename, "r");
+    if (!file) exit(EXIT_FAILURE);
+
+    char line[2048];
+    int idx = -1;
+
+    if (fgets(line, sizeof(line), file) == NULL) {
+        fclose(file);
+        exit(EXIT_FAILURE);
+    }
+
+    fgets(line, sizeof(line), file);
+    char *field;
+    field = strtok(line, ",");
+    while (line) {
+        idx++;
+        if (strcmp(field, field_name) == 0) break;
+        field = strtok(NULL, ",");
+    }
+
+    fclose(file);
+    if (idx > -1) return idx;
+    else exit(EXIT_FAILURE);
 }
